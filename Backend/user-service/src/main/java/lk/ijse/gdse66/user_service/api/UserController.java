547a5c1e518d47fcae06b61657e4d3b3ceb0a9bf
bethmij <bethmij@gmail.com>
2024-06-21 22:04:30 +0530

    user-service config

---
+++ Backend/user-service/src/main/java/lk/ijse/gdse66/user_service/api/UserController.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse66.user_service.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public String getMethod(){
+        return "request received";
+    }
+}
