4cbfbf3b3e16d548b3bf6d57e70485c1b577e257
bethmij <bethmij@gmail.com>
2024-06-28 09:59:43 +0530

    exceptions modified

---
+++ Backend/payment-service/src/main/java/lk/ijse/gdse66/payment_service/service/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse66.payment_service.service.exception;
+
+public class NotFoundException extends ServiceException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
